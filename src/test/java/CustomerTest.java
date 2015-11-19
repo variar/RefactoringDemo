f9a0ade3377ed19a7bb9eb84b965a7cafb007fd0
Anton Filimonov <anton.filimonov@gmail.com>
2015-11-19 15:38:02 +0300

    Add tests

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,18 +1,43 @@
 import com.scrumtrek.simplestore.Customer;
+import com.scrumtrek.simplestore.Rental;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CustomerTest {
 
+    private Customer m_Customer;
+
+    @Before
+    public void createCustomer() {
+        m_Customer = new Customer("name");
+    }
+
     @Test
     public void shouldReturnNameWhenGetName() {
-        Customer sut = new Customer("name");
-        Assert.assertEquals("name", sut.getName());
+        Assert.assertEquals("name", m_Customer.getName());
     }
 
     @Test
     public void shouldNotCrashWhenCallStatement() {
-        Customer sut = new Customer("name");
-        sut.Statement();
+        m_Customer.Statement();
+    }
+
+    @Test
+    public void shouldNotCrashWhenAddRental() {
+        Rental rental = org.mockito.Mockito.mock(Rental.class);
+        m_Customer.addRental(rental);
+    }
+
+    @Test
+    public void shouldState0AmountWhenCallStatementWithoutRentals() {
+        String statement = m_Customer.Statement();
+        Assert.assertTrue(statement.contains("Amount owed is 0"));
+    }
+
+    @Test
+    public void shouldState0FRPWhenCallStatementWithoutRentals() {
+        String statement = m_Customer.Statement();
+        Assert.assertTrue(statement.contains("You earned 0"));
     }
 }
