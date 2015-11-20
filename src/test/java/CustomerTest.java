1670af8a173e36dde69662d15ff55eba1f7fa81f
Anton Filimonov <anton.filimonov@gmail.com>
2015-11-20 12:59:35 +0300

    Add builder for rental

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -11,6 +11,31 @@ public class CustomerTest {
 
     private Customer m_Customer;
 
+    private class RentalBuilder {
+        private String m_movieName = "test movie";
+        private PriceCodes m_priceCode = PriceCodes.Regular;
+        private int m_days = 1;
+
+        public RentalBuilder SetMovieName(String name) {
+            m_movieName = name;
+            return this;
+        }
+
+        public RentalBuilder SetPriceCode(PriceCodes code) {
+            m_priceCode = code;
+            return this;
+        }
+
+        public RentalBuilder SetRentalDays(int days) {
+            m_days = days;
+            return this;
+        }
+
+        public Rental Build() {
+            return new Rental(new Movie(m_movieName, m_priceCode), m_days);
+        }
+    }
+
     @Before
     public void createCustomer() {
         m_Customer = new Customer("name");
@@ -56,49 +81,62 @@ public class CustomerTest {
 
     @Test
     public void shouldState2AmountWhenCallStatementWithRegularRentalFor1Day() {
-        m_Customer.addRental(new Rental(new Movie("123", PriceCodes.Regular), 1));
+        Rental rental = new RentalBuilder().Build();
+        m_Customer.addRental(rental);
         String statement = m_Customer.Statement();
         assertTrue(statement.contains("Amount owed is 2"));
     }
 
     @Test
     public void shouldState5AmountWhenCallStatementWithRegularRentalFor4Days() {
-        m_Customer.addRental(new Rental(new Movie("123", PriceCodes.Regular), 4));
+        Rental rental = new RentalBuilder().SetRentalDays(4).Build();
+        m_Customer.addRental(rental);
         String statement = m_Customer.Statement();
         assertTrue(statement.contains("Amount owed is 5"));
     }
 
     @Test
     public void shouldState1point5AmountWhenCallStatementWithChildrenRentalFor1Day() {
-        m_Customer.addRental(new Rental(new Movie("123", PriceCodes.Childrens), 1));
+        Rental rental = new RentalBuilder().SetPriceCode(PriceCodes.Childrens).Build();
+        m_Customer.addRental(rental);
         String statement = m_Customer.Statement();
         assertTrue(statement.contains("Amount owed is 1.5"));
     }
 
     @Test
     public void shouldState1point5AmountWhenCallStatementWithChildrenRentalFor4Day() {
-        m_Customer.addRental(new Rental(new Movie("123", PriceCodes.Childrens), 4));
+        Rental rental = new RentalBuilder()
+                .SetPriceCode(PriceCodes.Childrens)
+                .SetRentalDays(4)
+                .Build();
+        m_Customer.addRental(rental);
         String statement = m_Customer.Statement();
         assertTrue(statement.contains("Amount owed is 1.5"));
     }
 
     @Test
     public void shouldState3AmountWhenCallStatementWithNewRentalFor1Days() {
-        m_Customer.addRental(new Rental(new Movie("123", PriceCodes.NewRelease), 1));
+        Rental rental = new RentalBuilder().SetPriceCode(PriceCodes.NewRelease).Build();
+        m_Customer.addRental(rental);
         String statement = m_Customer.Statement();
         assertTrue(statement.contains("Amount owed is 3"));
     }
 
     @Test
     public void shouldState1FRPWhenCallStatementWithoutNewRelease() {
-        m_Customer.addRental(new Rental(new Movie("123", PriceCodes.Regular), 2));
+        Rental rental = new RentalBuilder().Build();
+        m_Customer.addRental(rental);
         String statement = m_Customer.Statement();
         assertTrue(statement.contains("You earned 1"));
     }
 
     @Test
     public void shouldState2FRPWhenCallStatementWithNewReleaseAndDays2() {
-        m_Customer.addRental(new Rental(new Movie("123", PriceCodes.NewRelease), 2));
+        Rental rental = new RentalBuilder()
+                .SetPriceCode(PriceCodes.NewRelease)
+                .SetRentalDays(2)
+                .Build();
+        m_Customer.addRental(rental);
         String statement = m_Customer.Statement();
         assertTrue(statement.contains("You earned 2"));
     }
